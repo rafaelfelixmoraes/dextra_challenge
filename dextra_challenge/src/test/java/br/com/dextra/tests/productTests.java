bd8664b301ffcfa76d424eb80ab1daebe233d795
rafae <rafae@StyleFelix-PC-2>
2018-04-13 20:48:27 -0300

    - Criação da classe de teste - Remoção de classes do pacote util não utilizadas

---
+++ dextra_challenge/src/test/java/br/com/dextra/tests/productTests.java
@@ -0,0 +1,14 @@
+package br.com.dextra.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class productTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
