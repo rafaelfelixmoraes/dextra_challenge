e4c1b04b5a04eca8024c54b859a0757337807d78
rafae <rafae@StyleFelix-PC-2>
2018-04-15 14:30:08 -0300

    Alterações:
    - Criação do front-end para consumo dos serviços
    - Alteração no back-end para permitir o consumo dos serviços

---
+++ dextra_challenge/src/main/webapp/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+   // declaração da variável
+   var valorEscolhido;
+ 
+   $("#sel1").change(function(){
+      // obtendo o valor do atributo value da tag option
+      valorEscolhido = $("#sel1 option:selected").val();
+      // exibindo uma janela com o valor selecionado
+      //alert (valorEscolhido);
+
+    	$.ajax({ 
+   			type: "GET",
+   			url: "http://localhost:8080/lanches/" + valorEscolhido,
+   			dataType: 'json',
+   			success: function(data){
+   				var lanche = "Lanche Escolhido: " + data.description + ", Preço: " + data.totalPrice;        
+     			alert(lanche);
+   			}
+		});
+   });
+});
