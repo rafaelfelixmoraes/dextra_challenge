a46ea192906901fc89c36800553f46e368a9e311
rafae <rafae@StyleFelix-PC-2>
2018-04-16 21:25:40 -0300

    - Incluida função no front para montar o json de pesquisa.

--- dextra_challenge/src/main/webapp/index.js
+++ dextra_challenge/src/main/webapp/index.js
@@ -1,13 +1,85 @@
 $(document).ready(function() {
+	var jsonObject = [];
+	$('#alface').change(function(){
+        if($("#alface").is(":checked")){
+            $("#child2").append("<select id='qtdAlface'></select>");
+            $("#qtdAlface").append("<option value='selecione' selected='selected'>Selecione...</option>");
+            $("#qtdAlface").append("<option value='1'>1</option>");
+            $("#qtdAlface").append("<option value='2'>2</option>");
+            $("#qtdAlface").append("<option value='3'>3</option>");
+            $("#qtdAlface").append("<option value='4'>4</option>");
+            $("#qtdAlface").append("<option value='5'>5</option>");
+            $("#qtdAlface").append("<option value='6'>6</option>");
+        } else {
+            $("#qtdAlface").remove();
+        }
+    });
     $('#bacon').change(function(){
         if($("#bacon").is(":checked")){
-            var s = $("")
             $("#child2").append("<select id='qtdBacon'></select>");
             $("#qtdBacon").append("<option value='selecione' selected='selected'>Selecione...</option>");
+            $("#qtdBacon").append("<option value='1'>1</option>");
+            $("#qtdBacon").append("<option value='2'>2</option>");
+            $("#qtdBacon").append("<option value='3'>3</option>");
+            $("#qtdBacon").append("<option value='4'>4</option>");
+            $("#qtdBacon").append("<option value='5'>5</option>");
+            $("#qtdBacon").append("<option value='6'>6</option>");
         } else {
             $("#qtdBacon").remove();
         }
     });
+    $('#hamburguerCarne').change(function(){
+        if($("#hamburguerCarne").is(":checked")){
+            $("#child2").append("<select id='qtdHamburguer'></select>");
+            $("#qtdHamburguer").append("<option value='selecione' selected='selected'>Selecione...</option>");
+            $("#qtdHamburguer").append("<option value='1'>1</option>");
+            $("#qtdHamburguer").append("<option value='2'>2</option>");
+            $("#qtdHamburguer").append("<option value='3'>3</option>");
+            $("#qtdHamburguer").append("<option value='4'>4</option>");
+            $("#qtdHamburguer").append("<option value='5'>5</option>");
+            $("#qtdHamburguer").append("<option value='6'>6</option>");
+        } else {
+            $("#qtdHamburguer").remove();
+        }
+    });
+    $('#ovo').change(function(){
+        if($("#ovo").is(":checked")){
+            $("#child2").append("<select id='qtdOvo'></select>");
+            $("#qtdOvo").append("<option value='selecione' selected='selected'>Selecione...</option>");
+            $("#qtdOvo").append("<option value='1'>1</option>");
+            $("#qtdOvo").append("<option value='2'>2</option>");
+            $("#qtdOvo").append("<option value='3'>3</option>");
+            $("#qtdOvo").append("<option value='4'>4</option>");
+            $("#qtdOvo").append("<option value='5'>5</option>");
+            $("#qtdOvo").append("<option value='6'>6</option>");
+        } else {
+            $("#qtdOvo").remove();
+        }
+    });
+    $('#queijo').change(function(){
+        if($("#queijo").is(":checked")){
+            $("#child2").append("<select id='qtdQueijo'></select>");
+            $("#qtdQueijo").append("<option value='selecione' selected='selected'>Selecione...</option>");
+            $("#qtdQueijo").append("<option value='1'>1</option>");
+            $("#qtdQueijo").append("<option value='2'>2</option>");
+            $("#qtdQueijo").append("<option value='3'>3</option>");
+            $("#qtdQueijo").append("<option value='4'>4</option>");
+            $("#qtdQueijo").append("<option value='5'>5</option>");
+            $("#qtdQueijo").append("<option value='6'>6</option>");
+        } else {
+            $("#qtdQueijo").remove();
+        }
+    });
+
+    clickButton = function (){
+    	var item = {};
+           item["id"] = parseInt($("#alface").val());
+           item["description"] = "Alface";
+           item["price"] = "0.40";
+           item["quantity"] = parseInt($("#qtdAlface option:selected").val());
+           jsonObject.push(item);
+    	console.log(jsonObject);
+    }
    // declaração da variável
    var valorEscolhido;
  
